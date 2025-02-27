3194321fb423b5547a566263ac9cccd11b8408e0
shuji_oishi <shuji_oishi@migiteq.com>
2025-02-27 17:25:46 +0900

    更新

--- api/hello.js
+++ api/hello.js
@@ -2,10 +2,10 @@ import mysql from 'mysql2/promise';
 
 // データベース接続設定
 const dbConfig = {
-  host: 52.35.92.59,
-  user: migiteq_remote,
-  password: migiteqdev0220!DB,
-  database: kids_dento_wp,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   port: process.env.MYSQL_PORT || 3306,
   ssl: process.env.MYSQL_SSL ? { rejectUnauthorized: false } : false
 };
